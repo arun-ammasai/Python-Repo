0015424661bb65bc08fdc2de10ce353ab777fef3
Arun <guideme360@gmail.com>
2019-01-06 21:01:47 +0530

    Database Operation

--- Python-Advance-Tutorial/DatabaseOperations/PythonDatabaseWithMysql.py
+++ Python-Advance-Tutorial/DatabaseOperations/PythonDatabaseWithMysql.py
@@ -1,6 +1,6 @@
 import pymysql
 
-# Database connection
+#Database connection
 
 def getConnection() :
     connection = pymysql.connect(host="localhost",user="root",passwd="",database="the_programming_guy")    
@@ -8,21 +8,39 @@ def getConnection() :
 
 
 # Create table 
-# connection = pymysql.connect(host="localhost",user="root",passwd="",database="the_programming_guy")
-# cursor = connection.cursor()
-# sql_create_table ="create table person(id int(20) primary key auto_increment, firstname char(20) not null,lastname char(20) not null)"
-# cursor.execute(sql_create_table)
-# connection.close()
+connection = pymysql.connect(host="localhost",user="root",passwd="",database="the_programming_guy")
+cursor = connection.cursor()
+sql_create_table ="create table person(id int(20) primary key auto_increment, firstname char(20) not null,lastname char(20) not null)"
+cursor.execute(sql_create_table)
+connection.close()
 
 # Insert Data
-# conn = getConnection()
-# cursor = conn.cursor()
-# i1 = "insert into person(firstname,lastname) values ('Nancymaria','Arulraj')"
-# i2 = "insert into person(firstname,lastname) values ('Paul','Coulson')"
-# cursor.execute(i1)
-# cursor.execute(i2)
-# conn.commit()
-# cursor.close()
+conn = getConnection()
+cursor = conn.cursor()
+i1 = "insert into person(firstname,lastname) values ('Nancymaria','Arulraj')"
+i2 = "insert into person(firstname,lastname) values ('Paul','Coulson')"
+cursor.execute(i1)
+cursor.execute(i2)
+conn.commit()
+cursor.close()
+
+# Update Data
+conn = getConnection()
+cursor = conn.cursor()
+sql_update_record ="update person set firstname='Nancy' where id =102"
+cursor.execute(sql_update_record)
+conn.commit()
+cursor.close()
+print("File Updated Successfully....")
+
+#Delete  Data
+conn = getConnection()
+cursor = conn.cursor()
+sql_delete_record ="delete from person where id =102"
+cursor.execute(sql_delete_record)
+conn.commit()
+cursor.close()
+print("Record Deleted Successfully....")
 
 # Fetch Data 
 conn = getConnection()
